09aba8a02e20934da51877f66d0ea13b371fbabf
Vinicius <viniciusmorais49@hotmail.com>
2022-06-14 11:59:33 -0300

    1

---
+++ monopoly/lib/pages/InicioEscolha.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class InicioEscolha extends StatefulWidget {
+  const InicioEscolha({Key? key}) : super(key: key);
+
+  @override
+  State<InicioEscolha> createState() => _InicioEscolhaState();
+}
+
+class _InicioEscolhaState extends State<InicioEscolha> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
