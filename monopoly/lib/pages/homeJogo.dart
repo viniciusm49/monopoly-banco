365546194db81e991d1961f6886af32ab4dc53fa
Vinicius <viniciusmorais49@hotmail.com>
2022-06-21 14:41:03 -0300

    10

--- monopoly/lib/pages/homeJogo.dart
+++ monopoly/lib/pages/homeJogo.dart
@@ -21,73 +21,150 @@ class _HomeJogoState extends State<HomeJogo> {
         appBar: AppBar(
           title: const Text("Jogo"),
         ),
-        body: Column(
-          children: [
-            Spacer(
-              flex: 1,
-            ),
-            Flexible(
-              child: ListView.builder(
-                  itemCount: jogoHome.listaJogadores.length,
-                  itemBuilder: (context, int index) {
-                    return Column(
-                      children: [
-                        Padding(
-                          padding: const EdgeInsets.all(8.0),
-                          child: InkWell(
-                            onLongPress: () {},
-                            child: Container(
-                              width: MediaQuery.of(context).size.width - 35,
-                              height: 60,
-                              decoration: BoxDecoration(
-                                borderRadius:
-                                    const BorderRadius.all(Radius.circular(12)),
-                                color: jogoHome.listaJogadores[index].cor,
-                                boxShadow: [
-                                  BoxShadow(
-                                    color: Colors.grey.withOpacity(0.5),
-                                    spreadRadius: 3,
-                                    blurRadius: 3,
-                                    offset: const Offset(
-                                        1, 1), // changes position of shadow
-                                  ),
-                                ],
-                              ),
-                              child: Row(
-                                children: [
-                                  const SizedBox(
-                                    width: 20,
-                                  ),
-                                  Text(
-                                      jogoHome.listaJogadores[index].nome
-                                          .toUpperCase(),
-                                      maxLines: 2,
-                                      style: const TextStyle(
-                                        color: Colors.white,
-                                        //fontFamily: 'KabelBd',
-                                        fontSize: 22,
-                                        fontWeight: FontWeight.bold,
-                                        shadows: <Shadow>[
-                                          Shadow(
-                                            offset: Offset(1.0, 1.0),
-                                            blurRadius: 5.0,
-                                            color: Color.fromARGB(255, 0, 0, 0),
-                                          ),
-                                        ],
-                                      )),
-                                  const Spacer(
-                                    flex: 1,
-                                  ),
-                                ],
-                              ),
-                            ),
-                          ),
+        body: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            children: [
+              const SizedBox(
+                height: 30,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Container(
+                    height: 90,
+                    width: 90,
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.grey.withOpacity(0.5),
+                          spreadRadius: 3,
+                          blurRadius: 3,
+                          offset:
+                              const Offset(1, 1), // changes position of shadow
                         ),
                       ],
-                    );
-                  }),
-            ),
-          ],
+                    ),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: const [
+                        Icon(
+                          Icons.history,
+                          size: 40,
+                          color: Colors.red,
+                        ),
+                        Text(
+                          "Histórico",
+                          style: TextStyle(
+                              color: Colors.red, fontWeight: FontWeight.bold),
+                        ),
+                      ],
+                    ),
+                  ),
+                  const SizedBox(
+                    width: 40,
+                  ),
+                  Container(
+                    height: 90,
+                    width: 90,
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.grey.withOpacity(0.5),
+                          spreadRadius: 3,
+                          blurRadius: 3,
+                          offset:
+                              const Offset(1, 1), // changes position of shadow
+                        ),
+                      ],
+                    ),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: const [
+                        Icon(
+                          Icons.play_circle,
+                          size: 40,
+                          color: Colors.red,
+                        ),
+                        Text(
+                          "Jogar Dados",
+                          style: TextStyle(
+                              color: Colors.red, fontWeight: FontWeight.bold),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+              const SizedBox(
+                height: 40,
+              ),
+              Flexible(
+                child: ListView.builder(
+                    itemCount: jogoHome.listaJogadores.length,
+                    itemBuilder: (context, int index) {
+                      return Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Column(
+                          children: [
+                            InkWell(
+                              onLongPress: () {},
+                              child: Container(
+                                width: MediaQuery.of(context).size.width - 35,
+                                height: 60,
+                                decoration: BoxDecoration(
+                                  borderRadius: const BorderRadius.all(
+                                      Radius.circular(12)),
+                                  color: jogoHome.listaJogadores[index].cor,
+                                  boxShadow: [
+                                    BoxShadow(
+                                      color: Colors.grey.withOpacity(0.5),
+                                      spreadRadius: 3,
+                                      blurRadius: 3,
+                                      offset: const Offset(
+                                          1, 1), // changes position of shadow
+                                    ),
+                                  ],
+                                ),
+                                child: Row(
+                                  children: [
+                                    const SizedBox(
+                                      width: 20,
+                                    ),
+                                    Text(
+                                        jogoHome.listaJogadores[index].nome
+                                            .toUpperCase(),
+                                        maxLines: 2,
+                                        style: const TextStyle(
+                                          color: Colors.white,
+                                          //fontFamily: 'KabelBd',
+                                          fontSize: 22,
+                                          fontWeight: FontWeight.bold,
+                                          shadows: <Shadow>[
+                                            Shadow(
+                                              offset: Offset(1.0, 1.0),
+                                              blurRadius: 5.0,
+                                              color:
+                                                  Color.fromARGB(255, 0, 0, 0),
+                                            ),
+                                          ],
+                                        )),
+                                  ],
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      );
+                    }),
+              ),
+              const SizedBox(
+                height: 15,
+              )
+            ],
+          ),
         ));
   }
 }
